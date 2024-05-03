0145c7ab2140ae6f5847c411fecb1d4d86713778
Thomas Eichinger <TIC@qlik.com>
2024-05-03 10:53:03 +0200

    final changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-    // site: 'https://teiching.github.io',
-    // base: 'my-astro-page',
+  site: 'https://teiching.github.io'
+  // base: 'my-astro-page',
+  ,
+  integrations: [preact()]
 });
