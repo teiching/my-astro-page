b1555e51e877b06eaaffeac9f7ddac522cbd6422
Thomas Eichinger <TIC@qlik.com>
2024-05-06 14:12:54 +0200

    changed actions yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://teiching.github.io'
-  // base: 'my-astro-page',
-  ,
+  site: 'https://teiching.github.io',
+  //base: 'my-astro-page',
+  
   integrations: [preact()]
 });
