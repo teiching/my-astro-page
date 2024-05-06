cd2e6f996f8914a1bdae4bede4f239506754afde
T. Eichinger <72072893+teiching@users.noreply.github.com>
2024-05-06 14:31:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://teiching.github.io',
-  base: 'my-astro-page',
+  base: '/my-astro-page',
   
   integrations: [preact()]
 });
